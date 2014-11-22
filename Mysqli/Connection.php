9611f5f9a9d8f74b72697d42f6427afeda3df94b
Payam Naderi <naderi.payam@gmail.com>
2014-11-22 14:44:57 +0330

    add mysql connection

--- Mysqli/Connection.php
+++ Mysqli/Connection.php
@@ -1,5 +1,5 @@
 <?php
-namespace Poirot\Database\Mysql;
+namespace Poirot\Database\Mysqli;
 
 use Poirot\Collection\Entity;
 use Poirot\Database\Connection\ConnectionInterface;
@@ -7,16 +7,50 @@ use Poirot\Database\Connection\ConnectionInterface;
 class Connection implements ConnectionInterface
 {
     /**
-     * Connect
+     * @var Entity
+     */
+    protected $config;
+
+    /**
+     * @var \mysqli
+     */
+    protected $engine;
+
+    /**
+     * @var \mysqli Initialized Config
+     */
+    protected $conn;
+
+    /**
+     * Construct
      *
-     * @param null|Entity $config Connection Configs
+     * @param Entity $config Connection Configuration
+     */
+    function __construct(Entity $config)
+    {
+        $this->setConfig($config);
+    }
+
+    /**
+     * Set Configuration Options
      *
-     * @throws \Exception
+     * @param Entity $config Config
+     *
      * @return $this
      */
-    function connect(Entity $config = null)
+    function setConfig(Entity $config)
     {
-        // TODO: Implement connect() method.
+        $this->config = $config;
+    }
+
+    /**
+     * Get Configuration Options
+     *
+     * @return Entity
+     */
+    function getConfig()
+    {
+        return $this->config;
     }
 
     /**
@@ -24,24 +58,79 @@ class Connection implements ConnectionInterface
      * - Initialize resource with Options
      * - connect
      *
+     * @param null|Entity $config Connection Configs
+     *
      * @return mixed
      */
-    function getConn()
+    function getConnection(Entity $config = null)
     {
-        // TODO: Implement getConn() method.
+        $this->conn = $this->getEngine();
+
+        // localize
+        $p = ($config) ?: $this->getConfig();
+
+        // given a list of key names, test for existence in $p
+        $findParameterValue = function (array $names) use ($p) {
+            foreach ($names as $name) {
+                if ($p->has($name)) {
+                    return $p->get($name);
+                }
+            }
+
+            return null;
+        };
+
+        $hostname = $findParameterValue(array('hostname', 'host'));
+        $username = $findParameterValue(array('username', 'user'));
+        $password = $findParameterValue(array('password', 'passwd', 'pw'));
+        $database = $findParameterValue(array('database', 'dbname', 'db', 'schema'));
+        $port     = $p->has('port') ? (int) $p->get('port') : null;
+        $socket   = $p->has('socket') ? $p->get('socket') : null;
+
+        if ($p->has('driver_options') && is_array($p->get('driver_options'))) {
+            foreach ($p->get('driver_options') as $option => $value) {
+                if (!is_string($option)) {
+                    continue;
+                }
+
+                $option = strtoupper($option);
+                if (!defined($option)) {
+                    continue;
+                }
+                $option = constant($option);
+
+                $this->conn->options($option, $value);
+            }
+        }
+
+        $this->conn->real_connect($hostname, $username, $password, $database, $port, $socket);
+
+        if ($this->conn->connect_error) {
+            throw new \RuntimeException(
+                'Connection error',
+                null,
+                new \ErrorException($this->conn->connect_error, $this->conn->connect_errno)
+            );
+        }
+
+        if ($p->has('charset')) {
+            $this->conn->set_charset($p->get('charset'));
+        }
+
+        return $this->conn;
     }
 
     /**
      * Close Connection
      *
-     * @return $this
      */
     function close()
     {
-        if ($this->resource instanceof \mysqli) {
-            $this->resource->close();
+        if ($this->engine instanceof \mysqli) {
+            $this->engine->close();
         }
-        unset($this->resource);
+
+        unset($this->engine);
     }
 
     /**
@@ -51,17 +140,7 @@ class Connection implements ConnectionInterface
      */
     function isConnected()
     {
-        return ($this->resource instanceof \mysqli);
-    }
-
-    /**
-     * Get Configs
-     *
-     * @return Entity
-     */
-    function config()
-    {
-        // TODO: Implement config() method.
+        return ($this->conn instanceof \mysqli);
     }
 
     /**
@@ -71,61 +150,12 @@ class Connection implements ConnectionInterface
      */
     function getEngine()
     {
-        if ($this->resource instanceof \mysqli) {
-            return $this;
+        if (!$this->engine instanceof \mysqli) {
+            $this->engine = new \mysqli();
+            $this->engine->init();
         }
 
-        // localize
-        $p = $this->connectionParameters;
-
-        // given a list of key names, test for existence in $p
-        $findParameterValue = function (array $names) use ($p) {
-            foreach ($names as $name) {
-                if (isset($p[$name])) {
-                    return $p[$name];
-                }
-            }
-            return;
-        };
-
-        $hostname = $findParameterValue(array('hostname', 'host'));
-        $username = $findParameterValue(array('username', 'user'));
-        $password = $findParameterValue(array('password', 'passwd', 'pw'));
-        $database = $findParameterValue(array('database', 'dbname', 'db', 'schema'));
-        $port     = (isset($p['port'])) ? (int) $p['port'] : null;
-        $socket   = (isset($p['socket'])) ? $p['socket'] : null;
-
-        $this->resource = new \mysqli();
-        $this->resource->init();
-
-        if (!empty($p['driver_options'])) {
-            foreach ($p['driver_options'] as $option => $value) {
-                if (is_string($option)) {
-                    $option = strtoupper($option);
-                    if (!defined($option)) {
-                        continue;
-                    }
-                    $option = constant($option);
-                }
-                $this->resource->options($option, $value);
-            }
-        }
-
-        $this->resource->real_connect($hostname, $username, $password, $database, $port, $socket);
-
-        if ($this->resource->connect_error) {
-            throw new Exception\RuntimeException(
-                'Connection error',
-                null,
-                new Exception\ErrorException($this->resource->connect_error, $this->resource->connect_errno)
-            );
-        }
-
-        if (!empty($p['charset'])) {
-            $this->resource->set_charset($p['charset']);
-        }
-
-        return $this;
+        return $this->engine;
     }
 }
  
